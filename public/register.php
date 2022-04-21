f39d6d6a6a49bcc34f9286a67819ff33eb6b0eed
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-04-21 16:17:17 +0300

    added registration

---
+++ public/register.php
@@ -0,0 +1,47 @@
+<?php
+include TEMPLATES_DIR.'header.php';
+include MODULES_DIR.'registration.php';
+
+$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+$pw = filter_input(INPUT_POST, 'pw', FILTER_SANITIZE_SPECIAL_CHARS);
+if (isset($_FILES['picture'])) {
+    $picture = filter_var_array($_FILES['picture']);
+}
+
+if (isset($username) && isset($pw)) {
+    try {
+        register($username, $pw, $picture);
+        header('Location: index.php');
+    } catch (Exception $e) {
+        echo '<div class="alert alert-danger" role="alert">'.$e->getMessage().'</div>';
+    }
+}
+?>
+
+<div>
+    <h3>Register</h3>
+</div>
+<form action="register.php" method="post" enctype="multipart/form-data">
+    <div>
+        <label for="username">Username:</label>
+        <input type="text" name="username" required/>
+    </div>
+    <div>
+        <label for="pw">Password:</label>
+        <input type="password" name="pw" required/>
+    </div>
+    <div>
+        <label for="picture">Profile Picture:</label>
+        <input type="file" name="picture"/>
+    </div>
+    <div>
+        <button type="submit">Register</button>
+    </div>
+</form>
+<div>
+    <p>Already a user? <a href="login.php">Login</a></p>
+</div>
+
+<?php
+include TEMPLATES_DIR.'footer.php';
+?>
---
+++ src/modules/registration.php
@@ -0,0 +1,35 @@
+<?php
+function register($username, $pw, $picture) {
+    require_once MODULES_DIR.'db.php';
+
+    if (!isset($username) || !isset($pw)) {
+        throw new Exception('Missing parameters.');
+    }
+
+    if (empty($username) || empty($pw)) {
+        throw new Exception('Username and password must be included.');
+    }
+
+    try {
+        $filename = $picture['name'];
+        $filetype = $picture['type'];
+        $path = PROFILE_PICTURES_DIR.basename($filename);
+        move_uploaded_file($picture['tmp_name'], $path);
+        $db = openDb();
+        $sql = 'insert into `user` (username, pw, picture) values (?, ?, ?)';
+        $statement = $db->prepare($sql);
+        $statement->bindParam(1, $username);
+        $hash_pw = password_hash($pw, PASSWORD_DEFAULT);
+        $statement->bindParam(2, $hash_pw);
+        $statement->bindParam(3, $filename);
+        $statement->execute();
+        $data = array('id' => $db->lastInsertId(), 'username' => $username, 'pw' => $hash_pw, 'picture' => $filename, 'filename' => $filename, 'type' => $filetype);
+        echo json_encode($data);
+    } catch (PDOException $e){
+        throw $e;
+    }
+}
+
+function unregister($id) {
+
+};
