9544661d401f0deb43ea3c1a8b3d387f4f0a39ea
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-04-26 11:37:42 +0300

    ability to change password

---
+++ public/changepw.php
@@ -0,0 +1,38 @@
+<?php
+include TEMPLATES_DIR.'header.php';
+include MODULES_DIR.'editprofile.php';
+
+$pw = filter_input(INPUT_POST, 'pw', FILTER_SANITIZE_SPECIAL_CHARS);
+
+if (isset($pw)) {
+    try {
+        changePassword($pw);
+        header('Location: mypage.php');
+    } catch (Exception $e) {
+        echo '<div class="alert alert-danger" role="alert">'.$e->getMessage().'</div>';
+    }
+}
+
+if (isset($_SESSION["username"])) { ?>
+
+<div class="container-fluid">
+    <div>
+        <h3>Change Password</h3>
+    </div>
+    <form action="changepw.php" method="post">
+        <div>
+            <label for="pw">Password:</label>
+            <input type="password" name="pw" required/>
+        </div>
+        <div>
+            <button type="submit">Save</button>
+        </div>
+    </form>
+    <div>
+        <button type="button"><a href="mypage.php">Cancel</a></button>
+    </div>
+</div>
+
+<?php }
+include TEMPLATES_DIR.'footer.php';
+?>
