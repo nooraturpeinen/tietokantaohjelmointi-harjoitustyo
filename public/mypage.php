52ecd16c08a6f666052f58c12bf0d615f1ca259c
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-04-27 17:42:04 +0300

    my page shows all users posts and their comments

--- public/mypage.php
+++ public/mypage.php
@@ -1,10 +1,13 @@
 <?php
 include TEMPLATES_DIR.'header.php';
+?>
+<style> <?php include BASE_DIR.'style.css' ?> </style>
+<?php
 include MODULES_DIR.'postandcomment.php';
 
 if (isset($_SESSION["username"])) { ?>
 
-<div class="container-fluid">
+<div class="container-fluid m-4">
     <div class="row">
         <div class="col-sm">
             <img src="../profile_pictures/<?php echo $_SESSION["picture"] ?>">
@@ -23,11 +26,21 @@ if (isset($_SESSION["username"])) { ?>
     <?php
     $posts = allUsersPosts();
     foreach ($posts as $p) {
-        echo "<div>
-            <h5>".$p["title"]."</h5>
-            <h6>@".$p["username"]."</h6>
-            <p>".$p["post"]."</p>
+        echo "<div id='allposts'>
+            <img src='../profile_pictures/".$p["picture"]."' id='postpicture'>
+            <h5 id='postusername'>".$p["username"]."</h5>
+            <hr>
+            <h6 id='posttitle'>".$p["title"]."</h6>
+            <p id='post'>".$p["post"]."</p>
         </div>";
+        $comments = allUsersPostsComments($p["id"]);
+        foreach ($comments as $c) {
+            echo "<div id='allcomments'>
+                <img src='../profile_pictures/".$c["picture"]."' id='commentpicture'>
+                <h6 id='commentusername'>".$c["username"]."</h6>
+                <p id='comment'>".$c["comment"]."</p>
+            </div>";
+        };
     }
     ?>
 </div>
