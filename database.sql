1c3f15bac551685ad5f02abe2a363c183da4eb18
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-04-27 17:36:25 +0300

    changed foreign keys to userid instead of username

--- database.sql
+++ database.sql
@@ -11,31 +11,23 @@ create table `user` (
     picture varchar(100)
 );
 
-insert into `user` (username, pw, picture) values ("noora", "noora", "blank_pfp.jpg");
-insert into `user` (username, pw, picture) values ("noorat", "noorat", "blank_pfp.jpg");
-
 create table post (
     id int primary key not null auto_increment,
-    username varchar(25) not null,
+    user_id int not null,
     title varchar(150) not null,
     post text not null,
     created timestamp default current_timestamp not null,
     updated datetime,
-    foreign key (username) references `user`(username)
+    foreign key (user_id) references `user`(id)
 );
 
-insert into post (username, title, post) values ("noora", "moikka", "eka postaukseni :)");
-
-create table comment (
+create table `comment` (
     id int primary key not null auto_increment,
     post_id int not null,
-    username varchar(25) not null,
+    user_id int not null,
     comment text not null,
     created timestamp default current_timestamp not null,
     updated datetime,
     foreign key (post_id) references post(id),
-    foreign key (username) references `user`(username)
+    foreign key (user_id) references `user`(id)
 );
-
-insert into comment (post_id, username, comment) values (1, "noorat", "kiva postaus!");
-insert into comment (post_id, username, comment) values (3, "noora", "jees!");
