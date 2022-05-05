f53cc4b071d34efed49e30b6f60500458a465dd1
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-05-05 13:49:38 +0300

    added an admin, a post and a comment

--- database.sql
+++ database.sql
@@ -11,6 +11,8 @@ create table `user` (
     picture varchar(100)
 );
 
+insert into `user` (username, pw, picture) values ("noora_admin", "abcde12345", "blank_pfp.png");
+
 create table post (
     id int primary key not null auto_increment,
     user_id int not null,
@@ -19,6 +21,8 @@ create table post (
     created timestamp default current_timestamp not null,
     foreign key (user_id) references `user`(id)
 );
+
+insert into post (user_id, title, post) values (1, "Welcome to our blog!", "Hi everyone! I'm Noora, one of the admins. Hope you enjoy our blog :)");
 
 create table `comment` (
     id int primary key not null auto_increment,
@@ -29,3 +33,5 @@ create table `comment` (
     foreign key (post_id) references post(id),
     foreign key (user_id) references `user`(id)
 );
+
+insert into `comment` (post_id, user_id, comment) values (1, 1, "If you have any questions, leave a comment under this post!");
